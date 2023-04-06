d8e38c29b364984f785051501de6895aee36679a
Jaeha Hwang <wogk216@naver.com>
2023-04-06 17:55:28 +0900

    URL Sesson 만들고 task 시작하기

--- MovieHjh/ViewController.swift
+++ MovieHjh/ViewController.swift
@@ -11,25 +11,52 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var table: UITableView!
     
+    let movieURL = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=e0d922798fe4b71963bb0052e9c4ad6b&targetDt=20230404"
+    
+    let name = ["영화이름1", "영화이름2", "영화이름3", "영화이름4", "영화이름5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
-
+        
+        // 1단계 : URL만들기
+        getData()
+    }
+    
+    func getData() {
+        if let url = URL(string: movieURL){
+            // 2단계 : URL Session 만들기
+            let session = URLSession(configuration: .default)
+            
+            // 3단계 : URL Session 인스턴스에게 task주기
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let JSONdata = data {
+                    print(JSONdata, response!)
+                    let dataString = String(data: JSONdata, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
+            // 4단계 :  task 시작하기 ( task.resume() )
+            task.resume()
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return name.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
-        cell.movieName.text = indexPath.description
+        cell.movieName.text = name[indexPath.row]
         
         return cell
     }
     
+
 }
-
-
-
