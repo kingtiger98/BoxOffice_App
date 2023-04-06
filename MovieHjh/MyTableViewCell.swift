7ec608ec892a6515df6f99afa54ac4f5ada096cf
Jaeha Hwang <wogk216@naver.com>
2023-04-06 15:43:58 +0900

    테이블뷰 설정

---
+++ MovieHjh/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  MovieHjh
+//
+//  Created by 황재하 on 4/6/23.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieHjh/ViewController.swift
+++ MovieHjh/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+
     }
-
-    // xcode 깃허브 기능 확인용 주석
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 
